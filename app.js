c8fba25eb7ae29777f8b93a17eda6d1562115f94
ibktommy <atomquartz@gmail.com>
2023-09-20 16:11:34 +0100

    Javascript code to make the navMenuBar clickable

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const navMenuBar = document.querySelector('.nav-center-menu-bar');
 const navMenuBarLines = document.querySelector('.line')
 const mobileNavlinks = document.querySelector('.mobile-navlinks');
 
-
-
 navMenuBar.addEventListener('click', () => {
   navMenuBar.classList.toggle('change')
   mobileNavlinks.classList.toggle('change')
