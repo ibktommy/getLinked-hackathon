f69a27b197b8898a9f4d2879ac7a80dca80f88fe
ibktommy <atomquartz@gmail.com>
2023-09-20 16:07:13 +0100

    Structured the Navbar, styled it and made it responsive in mobile view

---
+++ app.js
@@ -0,0 +1,11 @@
+// NAVMENU DISPLAY
+const navMenuBar = document.querySelector('.nav-center-menu-bar');
+const navMenuBarLines = document.querySelector('.line')
+const mobileNavlinks = document.querySelector('.mobile-navlinks');
+
+
+
+navMenuBar.addEventListener('click', () => {
+  navMenuBar.classList.toggle('change')
+  mobileNavlinks.classList.toggle('change')
+})
