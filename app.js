a3073e5d739801b241ae82c426d7bf4a640de16c
ibktommy <atomquartz@gmail.com>
2023-09-22 16:33:30 +0100

    structure faq-section, style it and make it responsive to screen sizes

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ navMenuBar.addEventListener('click', () => {
   navMenuBar.classList.toggle('change')
   mobileNavlinks.classList.toggle('change')
 })
+
+// FAQ ACCORDION
+const openIcon = document.querySelectorAll('.faq-lists .fa-plus')
+const closeIcon = document.querySelectorAll('.faq-lists .fa-minus')
+
+// Open Accordion
+openIcon.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    e.target.classList.add('display')
+    e.target.nextElementSibling.classList.remove('display')
+    e.target.parentElement.nextElementSibling.classList.remove('display')
+  })
+})
+
+// Close Accordion
+closeIcon
+.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    e.target.classList.add('display')
+    e.target.previousElementSibling.classList.remove('display')
+    e.target.parentElement.nextElementSibling.classList.add('display')
+  })
+})
