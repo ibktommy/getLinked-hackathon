0ee19057a08d73b0efad9d5fafc49dc0615c7930
ibktommy <atomquartz@gmail.com>
2023-09-23 11:27:15 +0100

    Add register page

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 // NAVMENU DISPLAY
 const navMenuBar = document.querySelector('.nav-center-menu-bar');
-const navMenuBarLines = document.querySelector('.line')
+const navMenuBarLines = document.querySelector('.line');
 const mobileNavlinks = document.querySelector('.mobile-navlinks');
 
 navMenuBar.addEventListener('click', () => {
-  navMenuBar.classList.toggle('change')
-  mobileNavlinks.classList.toggle('change')
-})
+	navMenuBar.classList.toggle('change');
+	mobileNavlinks.classList.toggle('change');
+});
 
 // FAQ ACCORDION
-const openIcon = document.querySelectorAll('.faq-lists .fa-plus')
-const closeIcon = document.querySelectorAll('.faq-lists .fa-minus')
+const openIcon = document.querySelectorAll('.faq-lists .fa-plus');
+const closeIcon = document.querySelectorAll('.faq-lists .fa-minus');
 
 // Open Accordion
 openIcon.forEach((icon) => {
-  icon.addEventListener('click', (e) => {
-    e.target.classList.add('display')
-    e.target.nextElementSibling.classList.remove('display')
-    e.target.parentElement.nextElementSibling.classList.remove('display')
-  })
-})
+	icon.addEventListener('click', (e) => {
+		e.target.classList.add('display');
+		e.target.nextElementSibling.classList.remove('display');
+		e.target.parentElement.nextElementSibling.classList.remove('display');
+	});
+});
 
 // Close Accordion
-closeIcon
-.forEach((icon) => {
-  icon.addEventListener('click', (e) => {
-    e.target.classList.add('display')
-    e.target.previousElementSibling.classList.remove('display')
-    e.target.parentElement.nextElementSibling.classList.add('display')
-  })
-})
+closeIcon.forEach((icon) => {
+	icon.addEventListener('click', (e) => {
+		e.target.classList.add('display');
+		e.target.previousElementSibling.classList.remove('display');
+		e.target.parentElement.nextElementSibling.classList.add('display');
+	});
+});
+
+// REGISTER FORM SUBMIT
+const registerForm = document.querySelector('.register-content');
+
+console.log(registerForm);
+
+// Close Register success
+const registerSuccess = document.querySelector('.register-success');
+const registerSucBtn = document.querySelector(
+	'.register-success-content button',
+);
+
+if (registerSucBtn !== null) {
+	registerSucBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		registerSuccess.classList.add('display');
+	});
+}
